81e87346e885d9eb917ed0c3aaa8303641dfc026
Tanni-Dey <tonnidey277615@gmail.com>
2023-03-01 22:09:34 +0600

    database-conected

--- index.js
+++ index.js
@@ -1,11 +1,38 @@
 const express = require("express");
 const cors = require("cors");
 const app = express();
+const { MongoClient, ServerApiVersion } = require("mongodb");
+require("dotenv").config();
 
 const port = process.env.PORT | 5000;
 
 app.use(cors());
 app.use(express.json());
+
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.1tyqf.mongodb.net/?retryWrites=true&w=majority`;
+const client = new MongoClient(uri, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+  serverApi: ServerApiVersion.v1,
+});
+
+async function run() {
+  try {
+    await client.connect();
+    // collections
+    const todosCollection = client.db("jobtask").collection("todos");
+
+    //Get all todos
+    app.get("/todos", async (req, res) => {
+      const query = {};
+      const cursor = todosCollection.find(query);
+      const allTodo = await cursor.toArray();
+      res.send(allTodo);
+    });
+  } finally {
+  }
+}
+run().catch(console.dir);
 
 app.get("/", (req, res) => {
   res.send("curlistic");
