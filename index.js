ad5cd913ab697d0d4e94ba090094eed19c2ae1a4
Tanni-Dey <tonnidey277615@gmail.com>
2023-03-03 22:17:26 +0600

    post api

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const cors = require("cors");
 const app = express();
-const { MongoClient, ServerApiVersion } = require("mongodb");
+const { MongoClient, ServerApiVersion, ObjectId } = require("mongodb");
 require("dotenv").config();
 
 const port = process.env.PORT | 5000;
@@ -28,6 +28,21 @@ async function run() {
       const cursor = todosCollection.find(query);
       const allTodo = await cursor.toArray();
       res.send(allTodo);
+
+      //Post todo
+      app.post("/todos", async (req, res) => {
+        const newTodo = req.body;
+        const addTodo = await todosCollection.insertOne(newTodo);
+        res.send(addTodo);
+      });
+      //delete todo
+      app.delete("/todo/:id", async (req, res) => {
+        const id = req.params.id;
+        const query = { _id: new ObjectId(id) };
+
+        const deleteTodo = await todosCollection.deleteOne(query);
+        res.send(deleteTodo);
+      });
     });
   } finally {
   }
